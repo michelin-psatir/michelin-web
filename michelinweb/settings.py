489875afde89c6f2caca02dee022b2e15b93ff15
stndaru <stefanus.ndaru@ui.ac.id>
2022-11-28 13:38:04 +0700

    Fix allowed host

--- michelinweb/settings.py
+++ michelinweb/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-x%=*=m0jsc9od_237mpyckur+_ylta=!_ba(tmu#93eqn-=4#)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
